2d7e79191743e63b21c4874cd106264de53458bf
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-20 12:45:35 +0100

    largenumbers

---
+++ largenumbers.js
@@ -0,0 +1,8 @@
+// create a new array (largeNumbers) that includes only the numbers greater than 10.
+
+const numbers = [15, 8, 23, 4, 42, 11];
+
+const largeNumbers = (numbers)=>{
+    return numbers.filter((number)=>number > 10)
+}
+console.log("largeNumbers:", largeNumbers(numbers));
